a611af4e509d722e2693a4df1ef06e8d8d4893db
martini5671 <koczot.martini@gmail.com>
2026-03-21 17:17:36 +0100

    * added some padding around router outlet in the menu bar. * added more fields to the task-from model and also added text fields for the task form

--- frontend/src/app/components/task-form/task-form.ts
+++ frontend/src/app/components/task-form/task-form.ts
@@ -1,10 +1,14 @@
 import {Component, signal} from '@angular/core';
-import {form, FormField, required} from '@angular/forms/signals';
+import {form, FormField, maxLength, minLength, required} from '@angular/forms/signals';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 
 @Component({
   selector: 'app-task-form',
   imports: [
-    FormField
+    FormField,
+    MatFormField,
+    MatLabel,
+    MatInput
   ],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
@@ -12,10 +16,14 @@ import {form, FormField, required} from '@angular/forms/signals';
 export class TaskForm {
 
   private taskModel = signal({
-    name: ""
+    title: "",
+    description: ""
   });
   protected taskForm = form(this.taskModel,
-    m => {
-      required(m.name)
+    model => {
+      required(model.title);
+      minLength(model.title, 3);
+
+      maxLength(model.description, 50);
     });
 }
